6f5c3214b8dcb84519be2423d5d6cd698707fd8a
SarthakDas4132 <sarthak.gfi@gmail.com>
2026-05-01 18:59:22 +0530

    Create TokenService.java

---
+++ app/src/main/java/com/project/back_end/services/TokenService.java
@@ -0,0 +1,13 @@
+package com.project.back_end.services;
+
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class TokenService {
+
+    public String generateToken() {
+        return UUID.randomUUID().toString();
+    }
+}
