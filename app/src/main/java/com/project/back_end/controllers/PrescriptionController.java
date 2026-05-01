838ba3a715cf10d315a348e00dfbbd13bf5160d0
SarthakDas4132 <sarthak.gfi@gmail.com>
2026-05-01 18:57:51 +0530

    Create PrescriptionController.java

---
+++ app/src/main/java/com/project/back_end/controllers/PrescriptionController.java
@@ -0,0 +1,26 @@
+package com.project.back_end.controllers;
+
+import com.project.back_end.models.Prescription;
+import com.project.back_end.services.PrescriptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/prescriptions")
+public class PrescriptionController {
+
+    @Autowired
+    private PrescriptionService prescriptionService;
+
+    @GetMapping
+    public List<Prescription> getAllPrescriptions() {
+        return prescriptionService.getAllPrescriptions();
+    }
+
+    @PostMapping
+    public Prescription createPrescription(@RequestBody Prescription prescription) {
+        return prescriptionService.savePrescription(prescription);
+    }
+}
