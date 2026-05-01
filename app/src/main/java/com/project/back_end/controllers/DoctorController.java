9ce07547654728ba4667b4c695d688af60809f6f
SarthakDas4132 <sarthak.gfi@gmail.com>
2026-05-01 18:50:58 +0530

    Update DoctorController.java

--- app/src/main/java/com/project/back_end/controllers/DoctorController.java
+++ app/src/main/java/com/project/back_end/controllers/DoctorController.java
@@ -1,1 +1,36 @@
+package com.project.back_end.controllers;
 
+import com.project.back_end.models.Doctor;
+import com.project.back_end.services.DoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/doctors")
+public class DoctorController {
+
+    @Autowired
+    private DoctorService doctorService;
+
+    @GetMapping
+    public List<Doctor> getAllDoctors() {
+        return doctorService.getAllDoctors();
+    }
+
+    @PostMapping
+    public Doctor addDoctor(@RequestBody Doctor doctor) {
+        return doctorService.saveDoctor(doctor);
+    }
+
+    @GetMapping("/{id}")
+    public Doctor getDoctorById(@PathVariable Long id) {
+        return doctorService.getDoctorById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteDoctor(@PathVariable Long id) {
+        doctorService.deleteDoctor(id);
+    }
+}
